c2bd674541828f26cc38a8efe8c209897a92a900
aleksanderfilek <56516786+aleksanderfilek@users.noreply.github.com>
2019-11-05 23:00:07 +0100

    Zajęcia 1

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    private bool jumpRequest = false; //zmienna przechowuje żądanie skoku
+
+    [SerializeField]
+    private float velocity = 10.0f; //przyśpieszenie skoku
+
+    [SerializeField]
+    private float fallMultiplier = 2.5f; //współczynnik grawitacji, czyli jak grawitacja to 9.81 to zwiększamy ją 1.5f * 9.81f, przez co postać szybciej spada
+
+    [SerializeField]
+    private float lowJumpMultiplier = 2.0f; //współczynnik grawitacji, w momencie gdy nie trzymamy spacji, a postać jest na etapie unoszenia. Jak nie jasne to priv
+
+    private Rigidbody rigidBody;
+
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump")) //sprawdzamy czy Jump(spacja) wciśnięty raz
+        {
+            jumpRequest = true;//ustawienie żadania skoku
+        }
+    }
+    void FixedUpdate() //Funkcja wywoływana w równych odstępach czasu, domyślnie działa 50 razy na sekunde. Służy do obliczeń fizycznych
+    {
+        //ten if odpowiada za nadanie energii skoku
+        if (jumpRequest)//sprawdzamy czy jest żądanie skoku
+        {
+            rigidBody.AddForce(Vector3.up * velocity, ForceMode.Impulse);//nadanie impulsu o wektorze skierowanym w góre (0,1,0) razy przyśpiesznie. 
+            //ForceMode.Impulse ustawia, że to pojedyńczy impuls (tylko raz dodaje przyśpieszenie w danym kierunku.
+            jumpRequest = false;//cofnięcie żądania skoku, bo wykonane
+        }
+
+        //te ify odpowiadają za wydłużenie i skrócenie skoku
+        if (rigidBody.velocity.y < 0.0f)//przyśpieszenie w osi Y jest ujemne gdy obiekt spada
+        {
+            rigidBody.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime; 
+            /*alternatywna i prostsza wersja powyższej linijki
+             * rigidBody.gravityScale = fallMultiplier
+             * poprostu zmieniamy mnożnik grawitacji
+             * */
+        }
+        else if (rigidBody.velocity.y > 0.0f && !Input.GetButton("Jump"))
+        {
+            rigidBody.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+            /*alternatywna wersja
+             * rigidBody.gravityScale = lowJumpMultiplier
+             */
+        }
+        /*ale przy alternatywnych wersja trzeba jeszcze wyzerować mnożnik grawitacji:
+         *else{
+         *    rigidBody.gravityScale = 1;
+         *}
+         * takie wykonanie wydaje mi się znacznie lepsze
+         */
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] //odpowiada za pokazywanie zmiennej w Inspectorze
+    private float speed = 50.0f;//zmienna prywatna odpowiadająca za prędkość poruszania
+    //gdyby private zamienić na public to nie potrzeba [SerializeField], ale przez to można 
+    //modyfikować zmienną przez inny skrypt
+    private Rigidbody rigidBody;//zmienna przechowująca odwołanie się do komponentu Rigidbody (referencja)
+
+
+    private void Awake() //funkcja wywołuje się raz przed rozpoczęciem gry, służy do przypisywania wartości zmiennych lub inicjalizacji obiektów
+    {
+        rigidBody = GetComponent<Rigidbody>(); //do zmiennej przypisujemy komponent z naszego obiektu 
+    }
+
+    void Update() //funkcja wywołuje się co klatke
+    {
+        float h = Input.GetAxis("Horizontal") * speed * Time.deltaTime; //tworzymy zmienną lokalną i przypisujemy do niej wartość wciśnietego przycisku (A = -1, D = 1), 
+        //mnożymy razy prędkość i czas trwania poprzedniej klatki, w celu sprawienia by ruch był gładszy, a nie skokowy
+        float v = Input.GetAxis("Vertical") * speed * Time.fixedTime; //podobnie jak wyżej tylko pobieramy wartości przycisków dla osi pionowej (W = 1, S = -1)
+
+        rigidBody.velocity = new Vector3(h, rigidBody.velocity.y, v);//do wektora przyśpieszenia w komponencie rigidbody, przypisujemy nowe wartości h i v. Instrukcja 
+        //rigidbody.velocity.y wstawia obecną wartosć przyśpieszenia w osi Y, po to by nie blokować skoku, gdyby wartość ustawić na 0, to nie można skakać
+    }
+}

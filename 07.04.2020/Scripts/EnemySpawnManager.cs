cb85f963dcd7910e8e24c049cfc5ccec45f4e088
Aleksander Filek <filekaleksander@gmail.com>
2020-04-15 14:07:17 +0200

    07.04.2020

---
+++ 07.04.2020/Scripts/EnemySpawnManager.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Unity.Entities;
+using Unity.Collections;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+namespace TD
+{
+    public class EnemySpawnManager : MonoBehaviour
+    {
+        private int startingEnemyAmount = 0; //for spawning enemies correctly
+        public int enemyAmount; //desired enemy amount
+        public float waitBetweenSpawnTime; //how long to wait between spawns
+        public Mesh enemyMesh; //what enemy would u want
+        public Material enemyMaterial; //in what color
+        public EntityManager spawnManager; //the thing handling all entities
+        public Entity player; //we need a reference to the player at all times
+        EntityArchetype enemyType = new EntityArchetype(); //two archetypes
+        EntityArchetype playerType = new EntityArchetype();
+
+        [SerializeField]
+        private Slider healthSlider; //for handling the slider
+
+        public static EnemySpawnManager instance;
+        public static EnemySpawnManager GetManager()
+        {
+            return instance;
+        }
+
+        private void Awake()
+        {
+            instance = this;
+        }
+
+        void Start()
+        {
+            spawnManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+            enemyType = spawnManager.CreateArchetype(
+                typeof(Translation),
+                typeof(EnemyMoveData)
+                ); //define enemy type
+
+            playerType = spawnManager.CreateArchetype(
+                typeof(PlayerData)
+                ); //define enemy type
+
+            player = spawnManager.CreateEntity(playerType); //create player from player type
+            spawnManager.SetComponentData(player, new PlayerData
+            {
+                health = 100,
+                money = 0
+            }); //set player values and slider values
+            healthSlider.maxValue = 100;
+            healthSlider.value = 100;
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                StartCoroutine(SpawnCoroutine()); //start the spawn coroutine, runs on separate thread
+            }
+        }
+
+        IEnumerator SpawnCoroutine()
+        {
+            while (startingEnemyAmount < enemyAmount) //until we reach desired amount, spawn enemies
+            {
+                SpawnEnemy();
+                yield return new WaitForSeconds(waitBetweenSpawnTime); //wait for the time
+                startingEnemyAmount++;
+            }
+            yield return 0;
+        }
+
+        public void SpawnEnemy()
+        {
+            Entity spawnedEnemy = spawnManager.CreateEntity(enemyType); //spawn enemy from enemytype and set desired values
+            spawnManager.SetComponentData(spawnedEnemy, new Translation
+            {
+                Value = new float3(
+                    PathFollowManager.instance.followPoints[0].position.x,
+                    PathFollowManager.instance.followPoints[0].position.y,
+                    PathFollowManager.instance.followPoints[0].position.z)
+            });
+            spawnManager.SetComponentData(spawnedEnemy, new EnemyMoveData
+            {
+                targetIndex = 1,
+                enemySpeed = 0.05f,
+                enemyMoveMinDistance = 0.3f,
+                enemyDamage = 5
+                //deltaTime = Time.deltaTime
+            });
+        }
+
+        public void UpdateHealthSlider()
+        {
+            healthSlider.value = spawnManager.GetComponentData<PlayerData>(player).health;
+            //check if dead
+        }
+    }
+}
---
+++ 07.04.2020/Scripts/TDComponents.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+using UnityEngine.Jobs;
+using Unity.Mathematics;
+using Unity.Jobs;
+
+namespace TD
+{
+    public struct EnemyMoveData : IComponentData //define the component that our enemies will have
+    { 
+        public int targetIndex;
+        public int enemyDamage;
+        public float enemySpeed;
+        public float enemyMoveMinDistance;
+        //public float deltaTime;
+    }
+
+    public struct PlayerData : IComponentData //define the component of the player
+    {
+        public int health;
+        public int money;
+    }
+
+    public class MeshRenderer : ComponentSystem //define the system which handles the rendering
+    { //drawmesh works only in main thread ???
+
+        protected override void OnUpdate()
+        {
+            Entities.ForEach((ref Translation transform) => { //look for all entities which have the translate and show the cubes on their position
+                EnemySpawnManager manager = EnemySpawnManager.GetManager();
+
+                Graphics.DrawMesh(
+                    manager.enemyMesh,
+                    transform.Value,
+                    Quaternion.identity,
+                    manager.enemyMaterial,
+                    0
+                    );
+            });
+        }
+    }
+
+    #region EnemyMover
+    public class EnemyMover : ComponentSystem
+    { //getposition works only in main thread ???
+
+        protected override void OnUpdate()
+        { //look for all entities that have the enemymovement and translation so we know they are enemies
+            Entities.ForEach((Entity e, ref Translation transform, ref EnemyMoveData moveData) =>
+            {
+                Entity entityToDestroy = e;
+                int damage = moveData.enemyDamage;
+                Vector3 targetPos = PathFollowManager.instance.followPoints[moveData.targetIndex].position;
+                Vector3 currentPos = transform.Value; //move them accordingly through the followpoints
+                Vector3 posDifference = (targetPos - currentPos);
+                if (posDifference.magnitude < moveData.enemyMoveMinDistance) //did the enemy reach the point?
+                {
+                    moveData.targetIndex++;
+                    moveData.targetIndex %= PathFollowManager.instance.followPoints.Count;
+                    if (moveData.targetIndex == 0) //if the enemy wants to go back to the start, it reached the end
+                    {
+                        //deal dmg to Player, destory entity
+                        Entities.ForEach((ref PlayerData player) =>
+                        {
+                            player.health -= damage;
+                            //destroy entity
+                            EnemySpawnManager.instance.spawnManager.DestroyEntity(entityToDestroy);
+                            EnemySpawnManager.instance.UpdateHealthSlider(); //update player hp
+                        });
+                    }
+                }
+                posDifference = posDifference.normalized * moveData.enemySpeed; //move enemy accordingly
+                transform.Value.x += posDifference.x;
+                transform.Value.y += posDifference.y;
+                transform.Value.z += posDifference.z;
+            });
+        }
+    }
+    #endregion
+}
